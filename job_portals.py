c289a1df2786431b8e7c2bfadf4fa613a24d9edb
developer <jack.daniel44@outlook.com>
2020-02-27 21:17:39 +0000

    init project

---
+++ job_portals.py
@@ -0,0 +1,56 @@
+job_portal_list = {'https://www.reed.co.uk/'
+                  ,'https://www.monster.co.uk/'
+                  ,'https://www.jobsite.co.uk/'
+                  ,'https://www.indeed.co.uk/'
+                  ,'http://jobs.telegraph.co.uk/'
+                  ,'https://www.agencycentral.co.uk/'
+                  ,'https://jobs.trovit.co.uk/'
+                  ,'https://www.cwjobs.co.uk/'
+                  ,'https://www.adzuna.co.uk/'
+                  ,'https://www.careerbuilder.co.uk/'}
+
+import requests
+from bs4 import BeautifulSoup as bs
+
+
+class Red:
+
+    def query_constructor(job: str, where: str) -> str:
+        """query constructor"""
+        base = 'https://www.reed.co.uk/jobs/'
+        title = f'{job.replace(" ", "-").lower()}'
+        cyti = f'-in-{where.lower()}'
+        query = base + title + cyti
+        return query
+
+    def get_requests(url: str) -> requests.Response:
+        """Gets a url and returns a response"""
+        return requests.get(url)
+
+    def scrap_h3(response: requests.Response) -> dict:
+        """Scrap the title, links from h3 bs elements"""
+        job_links = {}
+
+        # scrap
+        bs_object = bs(response.content, 'html.parser')
+        h3 = bs_object.find_all('h3')
+
+        print(type(h3))
+        # get the links and titles
+        for item in h3:
+            try:
+                job_links[item.text] = item.a['href']
+            except TypeError:
+                pass
+
+        # return dict[tile: link]
+        return job_links
+
+    # CONSTRUCTUR PATTERN
+    def get_jobs(title: str, location: str) -> dict:
+        query = Red.query_constructor(title, location)
+        response = Red.get_requests(query)
+        jobs = Red.scrap_h3(response)
+        return jobs
+
+
